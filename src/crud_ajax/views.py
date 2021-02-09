e2c612ae054ccfba1b2a285c5d7cbba4a5bfbef4
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-02-09 17:20:21 +0200

    created set model in crud page with ajax

--- src/crud_ajax/views.py
+++ src/crud_ajax/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+
 from .models import CrudUser
+
+from .models import CrudUser
+from django.views.generic import View
+from django.http import JsonResponse
+
 
 class CrudView(ListView):
     model = CrudUser
     template_name = 'crud_ajax/crud.html'
     context_object_name = 'users'
+
+
+
+
+class CreateCrudUser(View):
+    def  get(self, request):
+        name1 = request.GET.get('name', None)
+        address1 = request.GET.get('address', None)
+        age1 = request.GET.get('age', None)
+
+        obj = CrudUser.objects.create(
+            name = name1,
+            address = address1,
+            age = age1
+        )
+
+        user = {'id':obj.id,'name':obj.name,'address':obj.address,'age':obj.age}
+
+        data = {
+            'user': user
+        }
+        return JsonResponse(data)
--- src/django_curd/urls.py
+++ src/django_curd/urls.py
@@ -20,4 +20,5 @@ from crud_ajax import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('crud/',  views.CrudView.as_view(), name='crud_ajax'),
+    path('ajax/crud/create/',  views.CreateCrudUser.as_view(), name='crud_ajax_create'),
 ]
